0f1def137d50cfa648516cb71a38678223fcea14
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-09-27 05:15:12 +1000

    feat: add mappings for DarkClaims and MiningRig

--- src/DarkClaims.ts
+++ src/DarkClaims.ts
@@ -1,9 +1,49 @@
 import { ponder } from "@/generated";
+import { Inputs } from "./types";
+import { generateClaimId, generateUserId } from "./utils/idGenerator";
 
-ponder.on("DarkClaims:Claimed", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on("DarkClaims:Claimed", handleClaimed);
 
 ponder.on("DarkClaims:OwnershipTransferred", async ({ event, context }) => {
-  console.log(event.args);
+  // console.log(event.args);
 });
+
+
+async function handleClaimed({event,context}: Inputs): Promise<void> {
+  const { args, log, block, transaction } = event;
+  const { db, network, client, contracts } = context;
+  const {User: UserEntity, Claim: ClaimEntity} = db;
+  // treasury fee update
+  const {user, nonce, amount} = args;
+  // create user if doesn't exist
+  // if user doesn't exist, create a new user
+  const userId = generateUserId(user);
+  await UserEntity.upsert({
+    id: userId,
+    create: {
+      address: user,
+      totalMined: 0n,
+      isAllowlisted: false,
+      isHolderOfDark: false,
+      darkxbalance: 0n,
+      darkBalance: 0n, // dark balance is updated using handleDarkTransfer
+      fuelCellBalance: 0n,
+      totalMintedFuelCell: 0n,
+    },
+    update: {}
+  });
+
+  // create claim if doesn't exist
+  const claimId = generateClaimId(user, nonce);
+  await ClaimEntity.create({
+    id: claimId,
+    data: {
+      nonce: nonce,
+      userId: userId,
+      blocknumber: block.number,
+      timestamp: block.timestamp,
+      transactionHash: transaction.hash,
+      amount: amount
+    }
+  });
+}
--- src/MiningRig.ts
+++ src/MiningRig.ts
@@ -1,13 +1,56 @@
 import { ponder } from "@/generated";
+import { Inputs } from "./types";
+import { generateMineId, generateUserId } from "./utils/idGenerator";
 
 ponder.on("MiningRig:MerkleRootUpdated", async ({ event, context }) => {
-  console.log(event.args);
+  // console.log(event.args);
 });
 
-ponder.on("MiningRig:Mined", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on("MiningRig:Mined", handleMined);
 
 ponder.on("MiningRig:OwnershipTransferred", async ({ event, context }) => {
-  console.log(event.args);
+  // console.log(event.args);
 });
+
+
+async function handleMined({event,context}: Inputs): Promise<void> {
+  const { args, log, block, transaction } = event;
+  const { db, network, client, contracts } = context;
+  const {User: UserEntity, Mine: MineEntity} = db;
+  // treasury fee update
+  const {userAddress, nonce, amount, tokenInvested, token} = args;
+  // create user entity if it doesn't exist
+  // if user doesn't exist, create a new user
+  const userId = generateUserId(userAddress);
+  await UserEntity.upsert({
+    id: userId,
+    create: {
+      address: userAddress,
+      totalMined: amount,
+      isAllowlisted: false,
+      isHolderOfDark: false,
+      darkxbalance: 0n, // darkx balance is updated using handleDarkTransfer
+      darkBalance: 0n,
+      fuelCellBalance: 0n,
+      totalMintedFuelCell: 0n,
+    },
+    update: ({current}) => ({
+      totalMined: current.totalMined + amount,
+    })
+  })
+  // create mine entity if it doesn't exist
+  const mineId = generateMineId(userAddress, nonce);
+  await MineEntity.create({
+    id: mineId,
+    data: {
+      userId: userId,
+      blocknumber: event.block.number,
+      timestamp: event.block.timestamp,
+      transactionHash: event.transaction.hash,
+      amount,
+      tokenInvested,
+      token,
+    }
+  });
+}
+
