d548c8deaa370f43ec3a8cfe3195e83a05b8528e
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-09-26 23:46:36 +1000

    feat: add transfer event indexing

--- src/Antigravity.ts
+++ src/Antigravity.ts
@@ -1,23 +1,51 @@
 import { ponder } from "@/generated";
+import { Inputs } from "./types";
+import { generateAntigravityId, generateUserId } from "./utils/idGenerator";
 
-ponder.on("Antigravity:Approval", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on("Antigravity:Transfer", handleAntigravityTransfer);
 
-ponder.on("Antigravity:ApprovalForAll", async ({ event, context }) => {
-  console.log(event.args);
-});
+async function handleAntigravityTransfer({event,context}: Inputs): Promise<void> {
+  const { args, log, block, transaction } = event;
+  const { db, network, client, contracts } = context;
+  const {User: UserEntity, Antigravity: AntigravityEntity} = db;
+  // treasury fee update
+  const {from, to, id} = args;
 
-ponder.on(
-  "Antigravity:OwnershipHandoverCanceled",
-  async ({ event, context }) => {
-    console.log(event.args);
-  },
-);
+  // create antigravity entity
+  // the NFT is a soulbound NFT, hence the ownership will never change
+  const antigravityid = generateAntigravityId(contracts.Antigravity.address, id);
+  console.log(antigravityid);
 
-ponder.on(
-  "Antigravity:OwnershipHandoverRequested",
-  async ({ event, context }) => {
-    console.log(event.args);
-  },
-);
+  // if user doesn't exist, create a new user
+  const userId = generateUserId(to);
+  console.log(userId);
+  await UserEntity.upsert({
+    id: userId,
+    create: {
+      address: to,
+      totalMined: 0n,
+      isAllowlisted: false,
+      isHolderOfDark: false,
+      darkxbalance: 0n,
+      darkBalance: 0n,
+      fuelCellBalance: 0n,
+      totalMintedFuelCell: 0n,
+      antigravityId: antigravityid
+    },
+    update: {
+      antigravityId: antigravityid
+    }
+  });
+
+  await AntigravityEntity.create({
+    id: antigravityid,
+    data: {
+      contractAddress: contracts.Antigravity.address,
+      holderId: userId,
+      tokenId: id,
+      blocknumber: block.number,
+      timestamp: block.timestamp,
+      transactionHash: transaction.hash
+    }
+  });
+}
--- src/WishWellReceipt.ts
+++ src/WishWellReceipt.ts
@@ -1,23 +1,49 @@
 import { ponder } from "@/generated";
+import { Inputs } from "./types";
+import { generateUserId, generateWishWellId } from "./utils/idGenerator";
 
-ponder.on("WishWellReceipt:Approval", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on("WishWellReceipt:Transfer", handleWishWellReceiptTransfer);
 
-ponder.on("WishWellReceipt:ApprovalForAll", async ({ event, context }) => {
-  console.log(event.args);
-});
+async function handleWishWellReceiptTransfer({event,context}: Inputs): Promise<void> {
+  const { args, log, block, transaction } = event;
+  const { db, network, client, contracts } = context;
+  const {User: UserEntity, WishWell: WishWellEntity} = db;
+  // treasury fee update
+  const {from, to, id} = args;
 
-ponder.on(
-  "WishWellReceipt:OwnershipHandoverCanceled",
-  async ({ event, context }) => {
-    console.log(event.args);
-  },
-);
+  // create wishWell entity
+  // the NFT is a soulbound NFT, hence the ownership will never change
+  const wishWellid = generateWishWellId(contracts.WishWellReceipt.address, id);
 
-ponder.on(
-  "WishWellReceipt:OwnershipHandoverRequested",
-  async ({ event, context }) => {
-    console.log(event.args);
-  },
-);
+  // if user doesn't exist, create a new user
+  const userId = generateUserId(to);
+  await UserEntity.upsert({
+    id: userId,
+    create: {
+      address: to,
+      totalMined: 0n,
+      isAllowlisted: false,
+      isHolderOfDark: false,
+      darkxbalance: 0n,
+      darkBalance: 0n,
+      fuelCellBalance: 0n,
+      totalMintedFuelCell: 0n,
+      wishwellId: wishWellid
+    },
+    update: {
+      wishwellId: wishWellid
+    }
+  });
+
+  await WishWellEntity.create({
+    id: wishWellid,
+    data: {
+      contractAddress: contracts.WishWellReceipt.address,
+      holderId: userId,
+      tokenId: id,
+      blocknumber: block.number,
+      timestamp: block.timestamp,
+      transactionHash: transaction.hash
+    }
+  });
+}
