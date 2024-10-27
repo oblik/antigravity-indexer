646896da64ade995b1726dcb26dfea66d43549c2
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-10-27 12:23:29 +0530

    fix: add base as networks

--- ponder.config.ts
+++ ponder.config.ts
@@ -13,7 +13,7 @@ import { JourneyPhaseManagerAbi } from "./abis/JourneyPhaseManagerAbi";
 import { EvilAddressAbi } from "./abis/EvilAddressAbi";
 import { TreasuryAbi } from "./abis/TreasuryAbi";
 import { JackpotAbi } from "./abis/JackpotAbi";
-import { pulsechain, pulsechainV4 } from "viem/chains";
+import { base, pulsechain, pulsechainV4 } from "viem/chains";
 
 const MINING_RIG_ADDRESS=process.env.MINING_RIG_ADDRESS as string;
 const DARKX_ADDRESS=process.env.DARKX_ADDRESS as string;
@@ -44,6 +44,10 @@ export default createConfig({
       chainId: pulsechain.id,
       transport: http(process.env.PONDER_PULSECHAIN_URL)
     },
+    base: {
+      chainId: base.id,
+      transport: http(process.env.PONDER_BASE_URL)
+    },
     pulsechainV4: {
       chainId: pulsechainV4.id,
       transport: http(process.env.PONDER_PULSECHAIN_V4_URL)
@@ -51,80 +55,80 @@ export default createConfig({
   },
   blocks: {
     JourneyUpdateOnInterval: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       startBlock: parseInt(process.env.LATEST_BLOCK as string),
       interval: blockInterval
     },
   },
   contracts: {
     MiningRig: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: MINING_RIG_ADDRESS,
       abi: MiningRigAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA2 as string),
     },
     DarkX: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: DARKX_ADDRESS,
       abi: DarkXAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA2 as string),
     },
     Antigravity: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: ANTIGRAVITY_ADDRESS,
       abi: AntigravityAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA2 as string),
     },
     WishWellReceipt: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: WISHWELL_ADDRESS,
       abi: WishWellReceiptAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA1 as string),
     },
     DarkClaims: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: DARK_CLAIMS_ADDRESS,
       abi: DarkClaimsAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     Dark: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: DARK_ADDRESS,
       abi: DarkAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     LaunchControlCenter: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: LCC_ADDRESS,
       abi: LaunchControlCenterAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     FuelCell: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: FC_ADDRESS,
       abi: FuelCellAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     JourneyPhaseManager: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: JPM_ADDRESS,
       abi: JourneyPhaseManagerAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     EvilAddress: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: EVIL_ADDRESS,
       abi: EvilAddressAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     Treasury: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: TREASURY_ADDRESS,
       abi: TreasuryAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
     },
     Jackpot: {
-      network: process.env.NETWORK as "baseSepolia" | "sepolia",
+      network: process.env.NETWORK as "baseSepolia" | "sepolia" | "pulsechain" | "base" | "pulsechainV4",
       address: JACKPOT_ADDRESS,
       abi: JackpotAbi,
       startBlock: parseInt(process.env.START_BLOCK_ERA3 as string),
